7e08305a61a82bc0326a3f94b975b03a01b2d78b
Anıl ALTUNYURT <anilaltunyurt97@hotmail.com>
2021-01-28 03:19:45 +0300

    Add project files.

---
+++ HomeworkDictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkDictionary
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempTkey = keys;
+            keys = new Tkey[keys.Length + 1];
+            for (int i = 0; i < tempTkey.Length; i++)
+            {
+                keys[i] = tempTkey[i];
+
+            }
+            keys[keys.Length - 1] = key;
+
+
+            Tvalue[] tempTvalue = values;
+            values = new Tvalue[values.Length + 1];
+            for (int j = 0; j < tempTvalue.Length; j++)
+            {
+                values[j] = tempTvalue[j];
+
+            }
+            values[values.Length - 1] = value;
+        }
+
+
+        public Tkey[] key
+        {
+            get { return keys; }
+        }
+        public Tvalue[] value
+        {
+            get { return values; }
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+
+
+
+    }
+
+}
---
+++ HomeworkDictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeworkDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> month = new MyDictionary<int, string>();
+            month.Add(1, "Ocak");
+            month.Add(2, "Şubat");
+            month.Add(3, "Mart");
+            month.Add(4, "Nisan");
+            month.Add(5, "Mayıs");
+            month.Add(6, "Haziran");
+            month.Add(7, "Temmuz");
+            month.Add(8, "Ağustos");
+            month.Add(9, "Eylül");
+            month.Add(10, "Ekim");
+            month.Add(11, "Kasım");
+            month.Add(12, "Aralık");
+
+
+            int k = 0;
+
+            for (int m = 0; m < month.Length; m++)
+            {
+                Console.WriteLine(month.key[m] + " = " + month.value[m]);
+                k = m + 1;
+            }
+        }
+    }
+}
